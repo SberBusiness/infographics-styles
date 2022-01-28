bbc449e87d9a2b20bc211aabf633db94bba1507e
Dubsky Dmitry <Dubskiy.D.I@sberbank.ru>
2022-01-28 15:49:30 +0300

    Test publish script

--- scripts/copy.js
+++ scripts/copy.js
@@ -4,6 +4,4 @@ const distFolderPath = path.resolve(__dirname, '../dist');
 
 fs.copyFileSync(path.resolve(__dirname, '../LICENSE.txt'), path.resolve(distFolderPath, 'LICENSE.txt'));
 
-fs.copyFileSync(path.resolve(__dirname, '../package.json'), path.resolve(distFolderPath, 'package.json'));
-
 fs.copyFileSync(path.resolve(__dirname, '../README.md'), path.resolve(distFolderPath, 'README.md'));
